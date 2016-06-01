9f490fc4494bfb3af3cd8cd70a46d2f60e30913b
Kareem <kareemmuhammad924@gmail.com>
2016-06-01 10:42:45 +0200

    Started Working

---
+++ mhg_internalnet2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace mhg_internalnet2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ mhg_internalnet2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mhg_internalnet2.Startup))]
+namespace mhg_internalnet2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
